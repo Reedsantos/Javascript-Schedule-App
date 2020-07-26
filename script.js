221f657cdc65ced8b93794f8ce97213b023aa70e
Reed Santos <66302441+Reedsantos@users.noreply.github.com>
2020-07-26 16:37:16 -0400

    Add functions to show hour and save

--- script.js
+++ script.js
@@ -7,16 +7,46 @@ var toDoItems = [];
 
 function hourText() {
 
-    timeRow.each(function () {
-        var thisRow = $(this);
-        var thisRowHr = parseInt(thisRow.attr("data-hour"));
+  timeRow.each(function () {
+    var thisRow = $(this);
+    var thisRowHr = parseInt(thisRow.attr("data-hour"));
 
-        var todoObj = {
-            hour: thisRowHr,
-            text: "",
-        }
-        toDoItems.push(todoObj);
-    });
+    var todoObj = {
+      hour: thisRowHr,
+      text: "",
+    }
+    toDoItems.push(todoObj);
+  });
 
-    localStorage.setItem("todos", JSON.stringify(toDoItems));
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+};
+
+function rowColor() {
+  timeRow.each(function () {
+    var thisRow = $(this);
+    var thisRowHr = parseInt(thisRow.attr("data-hour"));
+
+    if (thisRowHr == currentHour) {
+      thisRow.addClass("present").removeClass("past future");
+    }
+    if (thisRowHr < currentHour) {
+      thisRow.addClass("past").removeClass("present future");
+    }
+    if (thisRowHr > currentHour) {
+      thisRow.addClass("future").removeClass("past present");
+    }
+  });
+}
+
+function save() {
+  var hourToUpdate = $(this).parent().attr("data-hour");
+  var itemToAdd = (($(this).parent()).children("textarea")).val();
+  for (var j = 0; j < toDoItems.length; j++) {
+    if (toDoItems[j].hour == hourToUpdate) {
+
+      toDoItems[j].text = itemToAdd;
+    }
+  }
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+  displaySchedule();
 };
